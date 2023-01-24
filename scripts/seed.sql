0c1560ad4129db7e3d90807dbaf0822181204d64
David <83370778+novakovicdavid@users.noreply.github.com>
2023-01-24 21:39:21 +0100

    Update integrated tests

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -5,6 +5,28 @@
 -- Dumped from database version 15.1
 -- Dumped by pg_dump version 15.1
 
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: figure
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
 --
 -- Name: figures; Type: TABLE; Schema: public; Owner: figure
 --
@@ -30,13 +52,11 @@ CREATE SEQUENCE public.figure_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
 --
 -- Name: figure_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: figure
 --
 
 ALTER SEQUENCE public.figure_id_seq OWNED BY public.figures.id;
-
 
 --
 -- Name: profiles; Type: TABLE; Schema: public; Owner: figure
@@ -46,7 +66,10 @@ CREATE TABLE public.profiles (
     id bigint NOT NULL,
     username text NOT NULL,
     display_name text,
-    user_id bigint
+    user_id bigint,
+    profile_picture text,
+    bio text,
+    banner text
 );
 
 --
@@ -122,25 +145,54 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.user_i
 -- Data for Name: figures; Type: TABLE DATA; Schema: public; Owner: figure
 --
 
-INSERT INTO public.figures (id, title, width, height, profile_id, url, description) VALUES (1, 'first', 1268, 951, 1, 'https://i.imgur.com/XpNCV7a.jpg', NULL);
+INSERT INTO public.figures (id, title, width, height, profile_id, url, description) VALUES (1, 'My other cat', 4128, 3096, 1, 'https://cdn.figure.novakovic.be/35357ff7-f1c0-4264-9c2a-98119ac6eaed', 'o.o');
+INSERT INTO public.figures (id, title, width, height, profile_id, url, description) VALUES (2, 'naps', 1125, 1160, 1, 'https://cdn.figure.novakovic.be/c5dc0e67-2928-4693-89b4-9afd7566e8ac', 'yes');
+INSERT INTO public.figures (id, title, width, height, profile_id, url, description) VALUES (3, 'leonardo', 530, 530, 2, 'https://cdn.figure.novakovic.be/e6906ab2-50b4-4a31-9dcb-96e90dcd5b41', 'dicaprio');
+INSERT INTO public.figures (id, title, width, height, profile_id, url, description) VALUES (4, 'Lazy cat', 1080, 1067, 1, 'https://cdn.figure.novakovic.be/17af1c06-df69-493a-aede-c7660c4d911c', 'he''s done');
+INSERT INTO public.figures (id, title, width, height, profile_id, url, description) VALUES (5, 'LMTS', 605, 605, 1, 'https://cdn.figure.novakovic.be/3ecd0561-c350-43c8-99d6-7fea4dd864b8', 'Limits Academy');
+INSERT INTO public.figures (id, title, width, height, profile_id, url, description) VALUES (6, 'Kestrel', 900, 600, 1, 'https://cdn.figure.novakovic.be/fdd38177-030e-4fc8-9e46-d58886557794', 'FTL: Faster Than Light');
+INSERT INTO public.figures (id, title, width, height, profile_id, url, description) VALUES (7, 'The default profile picture', 320, 320, 1, 'https://cdn.figure.novakovic.be/fbd19ed7-6f77-49e1-b54d-c913a75df62e', 'Given to every new user.');
 
 
 --
 -- Data for Name: profiles; Type: TABLE DATA; Schema: public; Owner: figure
 --
 
-INSERT INTO public.profiles (id, username, display_name, user_id) VALUES (DEFAULT, 'one', NULL, 1);
-INSERT INTO public.profiles (id, username, display_name, user_id) VALUES (DEFAULT, 'two', NULL, 2);
-INSERT INTO public.profiles (id, username, display_name, user_id) VALUES (DEFAULT, 'three', NULL, 3);
+INSERT INTO public.profiles (id, username, display_name, user_id, profile_picture, bio, banner) VALUES (4, 'four', 'Mr. Four', 4, 'https://cdn.figure.novakovic.be/profile_pictures/d6ce3718-6092-4d61-9a94-11399b915027', 'I can count to four.', 'https://cdn.figure.novakovic.be/banners/54cda2a9-aada-4d62-a2ce-cf86e3362304');
+INSERT INTO public.profiles (id, username, display_name, user_id, profile_picture, bio, banner) VALUES (1, 'one', 'hi', 1, 'https://cdn.figure.novakovic.be/profile_pictures/c52b7e30-e04d-4730-bcca-263caf8e162d', 'First user on this website. Test test test!!!', 'https://cdn.figure.novakovic.be/banners/2f0a720a-6bc6-468d-b7f2-eb8642a7cf84');
+INSERT INTO public.profiles (id, username, display_name, user_id, profile_picture, bio, banner) VALUES (2, 'two', NULL, 2, NULL, NULL, NULL);
+INSERT INTO public.profiles (id, username, display_name, user_id, profile_picture, bio, banner) VALUES (3, 'three', NULL, 3, NULL, NULL, NULL);
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: figure
 --
 
-INSERT INTO public.users (id, email, password, role) VALUES (DEFAULT, 'one@one.one', '$argon2id$v=19$m=8192,t=5,p=1$TAhaSdMMlP1AFODrj1Gvaw$lOhoEGfmQe2+SEDjjDlLU4EI/n3ij/vGRlUgUvED8Uc', 'user');
-INSERT INTO public.users (id, email, password, role) VALUES (DEFAULT, 'two@two.two', '$argon2id$v=19$m=8192,t=5,p=1$jY4QkgqqJTkt0blQ6TR/tg$XjA5OaSMuNmK98BdbW/UcDzRcD8q90Nailgoi9IP490', 'user');
-INSERT INTO public.users (id, email, password, role) VALUES (DEFAULT, 'three@three.three', '$argon2id$v=19$m=8192,t=5,p=1$ZHpPqdueTGnLg4eCP86j0Q$M4Wa686IxTfHV/ZvGDe6vwra1eeoRshviVxXxUTE2L4', 'user');
+INSERT INTO public.users (id, email, password, role) VALUES (1, 'one@one.one', '$argon2id$v=19$m=8192,t=5,p=1$Nup7xybNf9TvQSNvHAj1ng$MYlpQForpsxSM2p8Lskcnww0gX8MKuDAjLEyedMA//8', 'user');
+INSERT INTO public.users (id, email, password, role) VALUES (2, 'two@two.two', '$argon2id$v=19$m=8192,t=5,p=1$lNbswSxj/aLz6JDvSJIdOw$nCA3vNkWQrHdDODLD51M4V5uyeWObMtCrMP92wgHves', 'user');
+INSERT INTO public.users (id, email, password, role) VALUES (3, 'three@three.three', '$argon2id$v=19$m=8192,t=5,p=1$8MDHvZc89+IDwJBvODtJSw$rM3OXVO+BqLD/swRIZ/y/lSh9xQ7JEA+fvcwW3ZnqGI', 'user');
+INSERT INTO public.users (id, email, password, role) VALUES (4, 'four@four.four', '$argon2id$v=19$m=8192,t=5,p=1$nWYN3iWsF/RMcOjIioaSsA$q6d4D3Vj8xFgM3F9Bu7g+fZkoZctuKIqbIBJl+aPQhU', 'user');
+
+
+--
+-- Name: figure_id_seq; Type: SEQUENCE SET; Schema: public; Owner: figure
+--
+
+SELECT pg_catalog.setval('public.figure_id_seq', 7, true);
+
+
+--
+-- Name: profile_id_seq; Type: SEQUENCE SET; Schema: public; Owner: figure
+--
+
+SELECT pg_catalog.setval('public.profile_id_seq', 4, true);
+
+
+--
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: figure
+--
+
+SELECT pg_catalog.setval('public.user_id_seq', 4, true);
 
 
 --
@@ -187,7 +239,6 @@ ALTER TABLE ONLY public.figures
 
 ALTER TABLE ONLY public.profiles
     ADD CONSTRAINT profile_user_id_fk FOREIGN KEY (user_id) REFERENCES public.users(id);
-
 
 --
 -- PostgreSQL database dump complete
--- src/tests/test_routes_unauthenticated.rs
+++ src/tests/test_routes_unauthenticated.rs
@@ -10,10 +10,17 @@ use crate::server_errors::ServerError;
 pub async fn setup() -> Result<Router, Error> {
     let database = get_database_connection("postgres://postgres:mysecretpassword@localhost/postgres").await;
     let session_store = SessionStoreConnection::new("redis://localhost:6379").await;
+    let key_id = env::var("S3_APP_ID").expect("No S3_APP_ID env found");
+    let app_key = env::var("S3_APP_KEY").expect("No S3_APP_KEY env found");
+    let s3_region = env::var("S3_REGION").expect("No S3_REGION env found");
+    let bucket_endpoint = env::var("S3_ENDPOINT").expect("No S3_ENDPOINT env found");
+    let base_storage_url = env::var("S3_BASE_STORAGE_URL").expect("No S3_BASE_STORAGE_URL env found");
+    let bucket = env::var("S3_BUCKET").expect("No S3_BUCKET env found");
+    let content_store = S3Storage::new_store(key_id, app_key, s3_region, bucket_endpoint, base_storage_url, bucket);
     let domain = "localhost";
 
     Ok(create_app(
-        create_server_state(database, session_store, domain.to_string()),
+        create_server_state(database, session_store, content_store, domain.to_string()),
         create_app_cors(["http://localhost:3000".parse()?]),
         create_authentication_extension(),
     ))
@@ -44,16 +51,16 @@ async fn test_get_figure() -> Result<(), Error> {
     assert_eq!(body, json!({
         "figure": {
             "id": 1,
-            "title": "first",
-            "width": 1268,
-            "height": 951,
+            "title": "My other cat",
+            "width": 4128,
+            "height": 3096,
             "profile": {
                 "id": 1,
                 "username": "one",
-                "display_name": None as Option<String>,
+                "display_name": "hi",
             },
-            "url": "https://i.imgur.com/XpNCV7a.jpg",
-            "description": None as Option<String>
+            "url": "https://cdn.figure.novakovic.be/35357ff7-f1c0-4264-9c2a-98119ac6eaed",
+            "description": "o.o"
         }
     }));
     Ok(())
@@ -63,7 +70,7 @@ async fn test_get_figure() -> Result<(), Error> {
 async fn test_get_figure_non_existing() -> Result<(), Error> {
     let app = setup().await?;
     let response = app
-        .oneshot(Request::builder().method("GET").uri("/figures/2").body(Body::empty())?)
+        .oneshot(Request::builder().method("GET").uri("/figures/43545345345345").body(Body::empty())?)
         .await?;
     assert_eq!(response.status(), StatusCode::BAD_REQUEST);
     let body = hyper::body::to_bytes(response.into_body()).await?;
@@ -85,8 +92,8 @@ async fn test_signup_user() -> Result<(), Error> {
             .header(ACCEPT, mime::APPLICATION_JSON.as_ref())
             .body(Body::from(
                 json!({
-                "email": "four@four.four",
-                "username": "four",
+                "email": "five@five.five",
+                "username": "five",
                 "password": "password"
             }).to_string()
             ))?)
@@ -100,8 +107,8 @@ async fn test_signup_user() -> Result<(), Error> {
     let body: Value = serde_json::from_slice(&body)?;
     assert_eq!(body, json!({
         "profile": {
-            "id": 4,
-            "username": "four",
+            "id": 5,
+            "username": "five",
             "display_name": None as Option<String>
         }
     }));
